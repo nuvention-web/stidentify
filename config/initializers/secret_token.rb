e40eac2d8f7bc6b190fed99ce5f9c94dfee23b1f
Spencer Reinke <reinke.spencer@gmail.com>
2014-01-11 12:53:36 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Debatable::Application.config.secret_key_base = '6706deba1ebf09c4a178a68959c2ecb579bfe81af4135acba6d2fb7c3a1077f380fc1ebf14482ed761d47d5be6c414908b2b8ee94611fed59e31fed97e4680b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Debatable::Application.config.session_store :cookie_store, key: '_debatable_session'
