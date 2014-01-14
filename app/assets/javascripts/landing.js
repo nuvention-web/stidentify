ce54571237cdaa83c8e354c7cfab29aedf92a201
Brad Puder <bradleypuder2015@u.northwestern.edu>
2014-01-14 16:47:50 -0600

    panels are kindof sortof done

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,8 +1,14 @@
 function initializeLanding() {
-  $("button:contains('Learn More')").on("click", function(e) {
-        e.preventDefault();
-        $("#myModal").modal('show');
+  $(".landing-learn-more").on("click", function(e) {
+      e.preventDefault();
+      $("#myModal").modal('show');
   })
+
+  $(".landing-read-more").on("click", function(e) {
+    e.preventDefault();
+    $(".modal-body").prepend("<p>Sign up below to read the full article</p>");
+    $("#myModal").modal('show');
+  });
 
   $("form").on("submit", function(e) {
     e.preventDefault();
@@ -20,6 +26,18 @@ function initializeLanding() {
     } else {
       $('.modal-body').prepend("<p>That's not a valid email address!</p>");
     }
+  })
+
+  $(".thumbnail").on("mouseenter", function(){
+    $(this).css("background-color", "#5B6FC6");
+    $(this).find("p").css("color", "white");
+    $(this).find("h3").css("color", "white");
+  })
+
+  $(".thumbnail").on("mouseleave", function(){
+    $(this).css("background-color", "white");
+    $(this).find("p").css("color", "black");
+    $(this).find("h3").css("color", "black");
   })
 }
 
