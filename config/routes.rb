5d2d628266f7293ef9d933fffd3854468c111a10
Spencer Reinke <reinke.spencer@gmail.com>
2014-02-14 02:18:40 -0600

    Add StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Stidentify::Application.routes.draw do
+  root :to => "static_pages#home"
+  get "static_pages/home"
+  get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
