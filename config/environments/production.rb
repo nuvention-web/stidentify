ad3e33253731a2a88fbcf4a95a54bdc53ae74360
Spencer Reinke <reinke.spencer@gmail.com>
2014-02-17 22:11:18 -0600

    removed fontello reference

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Stidentify::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
-  # config.assets.compress = true
+  config.assets.compress = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
