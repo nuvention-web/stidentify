cef56e76e9e385ed20e4caa76de09274522c871f
Spencer Reinke <reinke.spencer@gmail.com>
2014-02-24 17:19:55 -0600

    remove cache

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-
-  validates_presence_of :email, :first_name, :last_name
-  validates_uniqueness_of :email
-
-  has_secure_password
-
-end
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Stidentify::Application.config.session_store :cookie_store, key: '_stidentify_session'
