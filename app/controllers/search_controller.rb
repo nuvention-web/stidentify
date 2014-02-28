91ccaa4a9902c71948341e99f87166461de5a7bb
Brad Puder <bradleypuder2015@u.northwestern.edu>
2014-02-28 00:28:06 -0600

    search controller fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -36,23 +36,23 @@ class SearchController < ApplicationController
   end
 
   def results
-    @location = params[:location].split(" ").join("+")
+    # @location = params[:location].split(" ").join("+")
 
-    # geocode_response = HTTParty.get(
-    #   "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=#{location}"
-    # )
+    # # geocode_response = HTTParty.get(
+    # #   "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=#{location}"
+    # # )
 
-    # miles = params[:radius].split(" ").first.to_i
-    # radius = miles * 1609
+    # # miles = params[:radius].split(" ").first.to_i
+    # # radius = miles * 1609
 
-    # @latlng = geocode_response["results"][0]["geometry"]["location"]
-    # location = "#{@latlng['lat']},#{@latlng['lng']}"
+    # # @latlng = geocode_response["results"][0]["geometry"]["location"]
+    # # location = "#{@latlng['lat']},#{@latlng['lng']}"
 
-    # @places_response = HTTParty.get(
-    #   "https://maps.googleapis.com/maps/api/place/nearbysearch/json?sensor=false&location=#{location}&radius=#{radius}&keyword=sexual+clinic&rankby=prominence&key=AIzaSyCjW847ACznKi7BuzYi_snkU5DeZFRfr3k"
-    # )["results"].to_json
+    # # @places_response = HTTParty.get(
+    # #   "https://maps.googleapis.com/maps/api/place/nearbysearch/json?sensor=false&location=#{location}&radius=#{radius}&keyword=sexual+clinic&rankby=prominence&key=AIzaSyCjW847ACznKi7BuzYi_snkU5DeZFRfr3k"
+    # # )["results"].to_json
 
-    render :results
+    # render :results
   end
 
 end
