ce6ffdcbcc48d7886347a5415497a386aa9820f5
Brad Puder <bradleypuder2015@u.northwestern.edu>
2014-04-06 02:16:34 -0500

    no clinic info

--- db/migrate/20140309032540_create_clinics.rb
+++ db/migrate/20140309032540_create_clinics.rb
@@ -1,16 +1,15 @@
 class CreateClinics < ActiveRecord::Migration
   def change
-    drop_table :clinics
-    create_table :clinics do |t|
-    	t.string :name
-    	t.string :address
-    	t.string :phone
-    	t.string :website
-    	t.string :fees
-    	t.string :hours
-    	t.string :lat
-    	t.string :lng
-    end
+    # create_table :clinics do |t|
+    # 	t.string :name
+    # 	t.string :address
+    # 	t.string :phone
+    # 	t.string :website
+    # 	t.string :fees
+    # 	t.string :hours
+    # 	t.string :lat
+    # 	t.string :lng
+    # end
   end
 end
 
