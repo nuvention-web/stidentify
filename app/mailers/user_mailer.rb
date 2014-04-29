f30c71e57476d12b64eff773c5bea4520d71a195
Spencer Reinke <reinke.spencer@gmail.com>
2014-04-29 11:56:16 -0500

    for heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
-class UserMailer < ActionMailer::Base
-  default from: "The STIdentify Team <team@stidentify.com>"
+# class UserMailer < ActionMailer::Base
+#   default from: "The STIdentify Team <team@stidentify.com>"
 
-  def signup(user)
-    @user = user
-    mail(to: @user.email, subject: 'Welcome to STIdentify')
-  end
+#   def signup(user)
+#     @user = user
+#     mail(to: @user.email, subject: 'Welcome to STIdentify')
+#   end
 
-end
+# end
