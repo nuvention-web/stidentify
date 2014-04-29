5ca159aaf73326a489eff17507b9d0c81214cd87
Brad Puder <bradleypuder2015@u.northwestern.edu>
2014-04-29 12:48:55 -0500

    commented out usermailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
     @user.document_id = response["document_id"]
 
     if @user.save
-      UserMailer.signup(@user).deliver
+      # UserMailer.signup(@user).deliver
       # session[:user_id] = @user.id
       # redirect_to user_path(@user)
       redirect_to thanks_path
